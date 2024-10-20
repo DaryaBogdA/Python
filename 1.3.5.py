ea45dfa5d5ded20cef5d93b5e290d98cfbfa590d
DaryaBogdA <babaikabarabaika@yandex.by>
2024-10-20 11:45:44 +0300

    1.3.4

--- 1.3.5.py
+++ 1.3.5.py
@@ -1,7 +1,15 @@
-dct = {
-	'year' : '2025',
-	'month': '12',
-	'day'  : '31',
+date= {
+  'year' : '2025',
+  'month': '12',
+  'day' : '31',
 }
-res = list(dct.values())
-print(res)
+
+formatted = ''
+part = ['year', 'month', 'day']
+
+for par in part:
+  formatted += date[par]
+  if par != 'day':
+    formatted += '-'
+
+print("'",formatted,"'") # Вывод: 2025-12-31
